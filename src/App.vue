bccbed355a0a79e7d53edf902a2b0b9b80375f8f
torchiba <r.chiba.torch.design@gmail.com>
2025-04-04 17:57:29 +0900

    add: computedで残件表示、watchおためし

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { reactive, computed, watch } from 'vue';
 let id:number = 0;
 const state = reactive({
   newTodo: '' as string,
@@ -17,8 +17,17 @@ const addTodo = () => {
   state.newTodo = ""
 }
 const removeTodo = (id:number) => {
-  state.todos = state.todos.filter((todo) => todo.id !== id)
+  state.todos = state.todos.filter(todo => todo.id !== id)
 }
+const remainingCount = computed(() => {
+  return state.todos.filter(todo => !todo.done).length;
+})
+watch(
+  () => state.todos.length,
+  (newValue, oldValue) => {
+    console.log(`value Change!: ${oldValue} -> ${newValue}`)
+  }
+)
 </script>
 
 <template>
@@ -32,6 +41,7 @@ const removeTodo = (id:number) => {
   </header>
 
   <main>
+    <p>合計：{{state.todos.length}}件 ｜ 未完了: {{ remainingCount }}件</p>
     <p v-for="todo in state.todos" :key="todo.id"><input type="checkbox" :id="'todo-' + todo.id" name="done" v-model="todo.done"/><label :for="'todo-' + todo.id" :class="{ checked: todo.done }">{{ todo.text }}</label><button @click="removeTodo(todo.id)">X</button></p>
   </main>
 </template>
