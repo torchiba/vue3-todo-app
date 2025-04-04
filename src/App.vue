3425865d3220e1def4e3d224bed1f8c59e9e22d5
torchiba <r.chiba.torch.design@gmail.com>
2025-04-04 15:58:57 +0900

    modify: 一部変数をstate としてまとめる

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 let id:number = 0;
-const newTodo = ref<string>('')
-const todos = ref<Todo[]>([])
+const state = reactive({
+  newTodo: '' as string,
+  todos: [] as Todo[]
+})
 type Todo = {
   id: number;
   text: string;
   done: boolean;
 }
+
 const addTodo = () => {
-  if (newTodo.value.trim() === '') return
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ""
+  if (state.newTodo.trim() === '') return
+  state.todos.push({ id: id++, text: state.newTodo, done: false })
+  state.newTodo = ""
 }
 const removeTodo = (id:number) => {
-  todos.value = todos.value.filter((todo) => todo.id !== id)
+  state.todos = state.todos.filter((todo) => todo.id !== id)
 }
 </script>
 
@@ -22,14 +25,14 @@ const removeTodo = (id:number) => {
   <header>
     <div class="wrapper">
       <form @submit.prevent="addTodo">
-        <input type="text" v-model="newTodo"/>
+        <input type="text" v-model="state.newTodo"/>
         <button>送信</button>
       </form>
     </div>
   </header>
 
   <main>
-    <p v-for="todo in todos" :key="todo.id"><input type="checkbox" :id="'todo-' + todo.id" name="done" v-model="todo.done"/><label :for="'todo-' + todo.id" :class="{ checked: todo.done }">{{ todo.text }}</label><button @click="removeTodo(todo.id)">X</button></p>
+    <p v-for="todo in state.todos" :key="todo.id"><input type="checkbox" :id="'todo-' + todo.id" name="done" v-model="todo.done"/><label :for="'todo-' + todo.id" :class="{ checked: todo.done }">{{ todo.text }}</label><button @click="removeTodo(todo.id)">X</button></p>
   </main>
 </template>
 
